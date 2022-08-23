84fe48421ddff4d88844b63f1db7ad77d9e73478
Enzo Lopez <enzolopez1610@gmail.com>
2022-08-23 18:19:37 -0300

    clase05 terminada

--- scripts.js
+++ scripts.js
@@ -16,6 +16,7 @@
         taskContent.appendChild(checkComplete());
         taskContent.appendChild(titleTask);
         task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
         list.appendChild(task);
     };
 
@@ -34,4 +35,17 @@
         element.classList.toggle('completeIcon');
         element.classList.toggle('far');
     }
+
+    const deleteIcon = () =>{
+        const i = document.createElement("i");
+        i.classList.add('fas','fa-trash-alt','trashIcon','icon');
+        i.addEventListener('click',deleteTask);
+        return i;
+    }
+
+    const deleteTask = (event) =>{
+        const parent = event.target.parentElement;
+        parent.remove();
+    }
+
 })();
